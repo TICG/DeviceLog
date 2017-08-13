0df76331d60a385c283f1c521dc37fea1a9e6f8d
CodeDead <admin@codedead.com>
2017-08-14 00:02:42 +0200

    * Added logging classes

---
+++ DeviceLog/Classes/Log/KeyboardLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceLog.Classes.Log
+{
+
+    internal class KeyboardLog : Log
+    {
+        private string data;
+
+        internal KeyboardLog()
+        {
+            data = "";
+        }
+
+        internal void AddKey(String key)
+        {
+            data += key;
+        }
+
+        internal string GetLog()
+        {
+            return data;
+        }
+    }
+}
---
+++ DeviceLog/Classes/Log/Log.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceLog.Classes.Log
+{
+    abstract class Log
+    {
+    }
+}
