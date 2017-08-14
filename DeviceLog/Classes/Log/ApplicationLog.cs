10cfdcca4f660126e140258c7f353668666657c3
CodeDead <admin@codedead.com>
2017-08-14 22:16:33 +0200

    * Added application logs * Added logcontroller

---
+++ DeviceLog/Classes/Log/ApplicationLog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace DeviceLog.Classes.Log
+{
+    /// <summary>
+    /// A class that represent a change in the application
+    /// </summary>
+    internal class ApplicationLog : Log
+    {
+        /// <summary>
+        /// The raw log data
+        /// </summary>
+        private readonly string _data;
+
+        /// <summary>
+        /// Initialize a new ApplicationLog object
+        /// </summary>
+        /// <param name="dateTime">A boolean to indicate whether the date and time should be logged or not</param>
+        /// <param name="data">The data that should be added to the log</param>
+        internal ApplicationLog(bool dateTime, string data)
+        {
+            _data = "";
+
+            if (dateTime)
+            {
+                _data += Environment.NewLine;
+                _data += "[" + DateTime.Now.ToString(CultureInfo.CurrentCulture) + "]";
+                _data += data;
+
+            }
+            else
+            {
+                _data += Environment.NewLine;
+                _data += data;
+            }
+        }
+
+        /// <summary>
+        /// Get the current raw string data of the log
+        /// </summary>
+        /// <returns>The raw string data of the log</returns>
+        internal string GetLog()
+        {
+            return _data;
+        }
+    }
+}
---
+++ DeviceLog/Classes/Log/LogController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DeviceLog.Classes.Log
+{
+    /// <summary>
+    /// This class holds a repository of all available logs
+    /// </summary>
+    internal class LogController
+    {
+        /// <summary>
+        /// The repository of all available logs that have been collected
+        /// </summary>
+        private readonly List<Log> _logList;
+
+        /// <summary>
+        /// Initialize a new LogController
+        /// </summary>
+        internal LogController()
+        {
+            _logList = new List<Log>();
+        }
+
+        /// <summary>
+        /// Add a new log to the existing log repository
+        /// </summary>
+        /// <param name="l">The unique log that should be added to the repository</param>
+        internal void AddLog(Log l)
+        {
+            if (l != null)
+            {
+                _logList.Add(l);
+            }
+        }
+
+        /// <summary>
+        /// Remove a log from the log repository
+        /// </summary>
+        /// <param name="l">The log that should be removed from the repository</param>
+        internal void RemoveLog(Log l)
+        {
+            if (_logList.Contains(l))
+            {
+                _logList.Remove(l);
+            }
+        }
+
+        /// <summary>
+        /// Get all available logs
+        /// </summary>
+        /// <returns>A list of all available logs</returns>
+        internal List<Log> GetLogs()
+        {
+            return _logList;
+        }
+
+        /// <summary>
+        /// Get all logs that are of the KeyboardLog type
+        /// </summary>
+        /// <returns>A list of collected Keyboard logs</returns>
+        internal List<Log> GetKeyboardLogs()
+        {
+            return _logList.Where(l => l.LogType == LogType.Keyboard).ToList();
+        }
+
+        /// <summary>
+        /// Get all logs that are of the FileSystemLog type
+        /// </summary>
+        /// <returns>A list of collected FileSystem logs</returns>
+        internal List<Log> GetFileSystemLogs()
+        {
+            return _logList.Where(l => l.LogType == LogType.FileSystem).ToList();
+        }
+    }
+}
