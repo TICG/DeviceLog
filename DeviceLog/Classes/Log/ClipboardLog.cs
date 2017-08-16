d5505779fdc576422215266ed1595c77631526b8
CodeDead <admin@codedead.com>
2017-08-16 23:10:13 +0200

    * Added Clipboard log * Added ClipboardHook * Added ClipboardModule * Changed log data

---
+++ DeviceLog/Classes/Log/ClipboardLog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceLog.Classes.Log
+{
+    internal class ClipboardLog : Log
+    {
+        private string _data;
+        private bool _logDate;
+        internal ClipboardLog(bool logDate)
+        {
+            LogType = LogType.Clipboard;
+            _data = "";
+            _logDate = logDate;
+        }
+
+        internal void AddData(string data)
+        {
+            if (_logDate)
+            {
+                _data += Environment.NewLine;
+                _data += "[" + DateTime.Now + "]" + data;
+            }
+            else
+            {
+                _data += Environment.NewLine;
+                _data += data;
+            }
+        }
+    }
+}
