bc981376f8975b79159a99b4358f7ee3430a2969
CodeDead <admin@codedead.com>
2018-01-09 00:51:20 +0100

    * Added ability to view application logs * Added ability to view keyboard logs * Added ability to view clipboard logs * Code improvements

---
+++ DeviceLog/Classes/Log/LogMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceLog.Classes.Log
+{
+    internal interface ILogMethods
+    {
+        void AddData(string data);
+        string GetData();
+    }
+}
