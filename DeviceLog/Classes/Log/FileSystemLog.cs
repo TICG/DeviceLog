781ac63c5d0875e5dbbb939cef94773f989f2df3
CodeDead <admin@codedead.com>
2018-01-10 01:15:18 +0100

    * Optimized date and time logging * Added filesystem watcher module * Added scrollbars to log window

---
+++ DeviceLog/Classes/Log/FileSystemLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceLog.Classes.Log
+{
+    internal class FileSystemLog : Log
+    {
+        /// <summary>
+        /// Initialize a new FileSystemLog
+        /// </summary>
+        internal FileSystemLog()
+        {
+            LogType = LogType.Keyboard;
+            Data = "";
+        }
+
+        /// <summary>
+        /// Add new data to the current log
+        /// </summary>
+        /// <param name="key">The data that should be added to the log</param>
+        internal new void AddData(string key)
+        {
+            Data += key;
+            LogUpdatedEvent?.Invoke(Data);
+        }
+    }
+}
